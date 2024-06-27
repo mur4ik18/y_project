6142b643981bc23e9d95b2328c8945c982094759
mur4ik18 <o.o.kotov1999@gmail.com>
2024-06-27 12:42:05 +0200

    Open file and read hexa

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use std::env;
 use std::fs;
 
+fn read_file(file_path: String) {
+    let bytes = fs::read(file_path.to_owned()).unwrap();
+    for byte in bytes.iter() {
+        print!("{:X} ", byte);
+    }
+    println!();
+}
+
+fn get_arguments() -> Vec<String> {
+    let args: Vec<String> = env::args().collect();
+    args
+    //println!("Paht : {file_path}");
+    //file_path
+}
 
 fn main() {
     println!("I will open file");
-    let file_path = "test.txt";
-
-    let content = fs::read_to_string(file_path)
-    .expect("Should have been able to read the file");
-
-    println!("With text:\n{content}");
+    let args: Vec<String> = get_arguments();
+    read_file(args[1].clone());
 }
