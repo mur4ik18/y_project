e8f48733000b4fafb3719a98f5a0dbde5fb0f99d
mur4ik18 <o.o.kotov1999@gmail.com>
2024-06-27 10:57:35 +0200

    Add file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::fs;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("I will open file");
+    let file_path = "test.txt";
+
+    let content = fs::read_to_string(file_path)
+    .expect("Should have been able to read the file");
+
+    println!("With text:\n{content}");
 }
