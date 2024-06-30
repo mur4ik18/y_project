b9bf79a5cb29941c44235c331051610956c86484
nvonaesch <nicolas.vonaesch1@gmail.com>
2024-07-01 01:22:03 +0200

    reformat code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,45 @@
 use std::env;
 use std::fs;
 
+/************************************************************************************/
+/********************************File Signatures*************************************/
+/************************************************************************************/
+
 struct Signature<'a> {
     name: &'a str,
     signature: &'a [u8],
 }
+
+const SIGNATURES: [Signature; 7] = [
+    Signature {
+        name: "DOS MZ executable",
+        signature: b"\x4D\x5A",
+    },
+    Signature {
+        name: "Executable and Linkable Format (ELF)",
+        signature: b"\x7F\x45\x4C\x46",
+    },
+    Signature {
+        name: "Mach-O binary (32-bit)",
+        signature: b"\xFE\xED\xFA\xCE",
+    },
+    Signature {
+        name: "Mach-O binary (64-bit)",
+        signature: b"\xFE\xED\xFA\xCF",
+    },
+    Signature {
+        name: "Mach-O binary (reverse byte ordering scheme, 32-bit)",
+        signature: b"\xCE\xFA\xED\xFE",
+    },
+    Signature {
+        name: "Mach-O binary (reverse byte ordering scheme, 64-bit)",
+        signature: b"\xCF\xFA\xED\xFE",
+    },
+    Signature {
+        name: "Java class file, Mach-O Fat Binary",
+        signature: b"\xCA\xFE\xBA\xBE",
+    },
+];
 
 /*********************************************************************************/
 /********************************PE structure*************************************/
@@ -102,37 +137,6 @@ struct FileInfoMachO<'a> {
 fn reverse_bytes<T: Clone>(slice: &[T]) -> Vec<T> {
     slice.iter().cloned().rev().collect()
 }
-
-const SIGNATURES: [Signature; 7] = [
-    Signature {
-        name: "DOS MZ executable",
-        signature: b"\x4D\x5A",
-    },
-    Signature {
-        name: "Executable and Linkable Format (ELF)",
-        signature: b"\x7F\x45\x4C\x46",
-    },
-    Signature {
-        name: "Mach-O binary (32-bit)",
-        signature: b"\xFE\xED\xFA\xCE",
-    },
-    Signature {
-        name: "Mach-O binary (64-bit)",
-        signature: b"\xFE\xED\xFA\xCF",
-    },
-    Signature {
-        name: "Mach-O binary (reverse byte ordering scheme, 32-bit)",
-        signature: b"\xCE\xFA\xED\xFE",
-    },
-    Signature {
-        name: "Mach-O binary (reverse byte ordering scheme, 64-bit)",
-        signature: b"\xCF\xFA\xED\xFE",
-    },
-    Signature {
-        name: "Java class file, Mach-O Fat Binary",
-        signature: b"\xCA\xFE\xBA\xBE",
-    },
-];
 
 // context
 struct Ctx {
