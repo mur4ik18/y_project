be120a821721ffef3542c951169c19d2822375ee
mur4ik18 <o.o.kotov1999@gmail.com>
2024-07-02 16:53:36 +0200

    d

--- src/aarch_opcodes_instructions.rs
+++ src/aarch_opcodes_instructions.rs
@@ -4,7 +4,17 @@ use std::collections::HashMap;
 pub fn get_opcodes() -> HashMap {
     let ARM = HashMap::from([
         (0x8B, Opcode("ADD", Addressing::IMMEDIATE)),
+        //(0x8B, Opcode("ADD", Addressing::RTR),
+        (0xCB, Opcode("SUB", Addressing::RTR)),
+        (0x9B, Opcode("MUL", Addressing::RTR)),
+        //
+        (0x8A, Opcode("AND", Addressing::RTR)),
+        (0xAA, Opcode("ORR", Addressing::RTR)),
+        (0xCA, Opcode("EOR", Addressing::RTR)),
+        // Movement Instructions
         (0xD2, Opcode("MOV", Addressing::IMMEDIATE)),
         (0xAA, Opcode("MOV", Addressing::RTR)),
+        (0xF9, Opcode("LDR", Addressing::IMMEDIATE)),
+        (0xAA, Opcode("STR", Addressing::RTR)),
     ]);
 }
